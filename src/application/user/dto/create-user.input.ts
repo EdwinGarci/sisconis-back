76daafa23194b676dcb3610681d29b12849da8fd
EdwinGarci <edwingarciatavara@gmail.com>
2025-05-29 22:56:27 -0500

    refactor: enhance documentation for CreateUserInput DTO

--- src/application/user/dto/create-user.input.ts
+++ src/application/user/dto/create-user.input.ts
@@ -1,9 +1,29 @@
 import { Role } from "@domain/user/entities/user.entity";
 
+/**
+ * The input data transfer object for creating a user.
+ * 
+ * @interface CreateUserInput
+ */
 export interface CreateUserInput {
+    /**
+     * The name of the user.
+     */
     name: string;
+    /**
+     * The lastname of the user.
+     */
     lastname: string;
+    /**
+     * The username of the user.
+     */
     username: string;
+    /**
+     * The role of the user.
+     */
     role: Role;
+    /**
+     * Optional password - if not provided, a default password will be generated.
+     */
     password?: string;
 }
