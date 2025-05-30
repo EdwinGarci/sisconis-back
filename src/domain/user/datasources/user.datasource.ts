f77b66ad0ff52613d65cccc0c15aee7aa04dcb19
EdwinGarci <edwingarciatavara@gmail.com>
2025-05-29 22:36:33 -0500

    refactor: add delete methods to user datasource and repository interfaces

--- src/domain/user/datasources/user.datasource.ts
+++ src/domain/user/datasources/user.datasource.ts
@@ -1,9 +1,41 @@
 import { UserEntity } from "../entities/user.entity";
 
+/**
+ * An abstract datasource interface for managing user entities.
+ * Provides methods to perform CRUD operations on users.
+ */
 export abstract class UserDatasource {
+    /**
+     * Retrieves all users from the datasource.
+     * @returns A promise that resolves to an array of UserEntity objects.
+     */
     abstract getUsers(): Promise<UserEntity[]>;
+
+    /**
+     * Retrieves a user by their ID.
+     * @param id The ID of the user to retrieve.
+     * @returns A promise that resolves to a UserEntity object.
+     */
     abstract getUserById(id: number): Promise<UserEntity>;
+
+    /**
+     * Creates a new user and returns the created user.
+     * @param user - User data to create a new user.
+     * @returns The created user.
+     */
     abstract createUser(user: UserEntity): Promise<UserEntity>;
+
+    /**
+     * Updates an existing user and returns the updated user.
+     * @param user - User data to update an existing user.
+     * @returns The updated user.
+     */
     abstract updateUser(user: UserEntity): Promise<UserEntity>;
+
+    /**
+     * Deletes a user by their ID.
+     * @param id The ID of the user to delete.
+     * @returns A promise that resolves when the user is deleted.
+     */
     abstract deleteUser(id: number): Promise<void>;
 }
