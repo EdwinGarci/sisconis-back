0565c4a65db70982a4cb7b042b4dc50953460ced
EdwinGarci <edwingarciatavara@gmail.com>
2025-05-30 22:26:34 -0500

    refactor: simplify user use cases and improve type consistency

--- src/application/user/use-cases/get-users.use-case.ts
+++ src/application/user/use-cases/get-users.use-case.ts
@@ -1,32 +1,10 @@
-import { Result } from '../../shared/core/Result';
-import { Either, left, right } from '../../shared/core/Either';
-import { User } from '@domain/user/entities/user.entity';
-import { UserRepository } from '@domain/user/repositories/user.repository';
-import { UserResponseDTO } from '../dto/user.response.dto';
-
-export type GetUsersUseCaseResult = Either<
-  Error,
-  Result<UserResponseDTO[]>
->;
+import { UserEntity } from "@domain/user/entities/user.entity";
+import { UserRepository } from "@domain/user/repositories/user.repository";
 
 export class GetUsersUseCase {
-  constructor(
-    private readonly userRepository: UserRepository
-  ) {}
+	constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(): Promise<GetUsersUseCaseResult> {
-    try {
-      const users = await this.userRepository.findAll();
-      const userResponses = users.map(user => ({
-        id: user.getId(),
-        name: user.getName(),
-        lastname: user.getLastname(),
-        username: user.getUsername(),
-        role: user.getRole()
-      }));
-      return right(Result.ok(userResponses));
-    } catch (error) {
-      return left(error);
-    }
-  }
+	async execute(): Promise<UserEntity[]> {
+		return this.userRepository.index();
+	}
 }
